991e8d4b2e11c92d0efbea3ee0c1a018baf60d04
Che Sun <jazzboysc@gmail.com>
2018-07-31 12:36:15 +0800

    ....

--- Character/SECharacterDeform.py
+++ Character/SECharacterDeform.py
@@ -1,4 +1,18 @@
 import maya.cmds as cmds
+import os
+from ..ThirdParty import bSkinSaver
+
+skinWeightsDir = 'Weights/SkinCluster'
+skinWeightsExt = '.swt'
 
 def build(baseRig, mainProjectPath, sceneScale):
     pass
+
+def saveSkinWeights(characterName, mainProjectPath, geoList = []):
+    for obj in geoList:
+        # Weights file path.
+        weightFilePath = os.path.join(mainProjectPath, characterName, skinWeightsDir, obj + skinWeightsExt)
+
+        # Save skin weights.
+        cmds.select(obj)
+        bSkinSaver.bSaveSkinValues(weightFilePath)
