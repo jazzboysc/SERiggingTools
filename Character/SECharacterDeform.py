7af4c1669819c366a04a1ced8088ae04a1305183
Che Sun <jazzboysc@gmail.com>
2018-07-31 14:55:10 +0800

    ....

--- Character/SECharacterDeform.py
+++ Character/SECharacterDeform.py
@@ -16,3 +16,31 @@ def saveSkinWeights(characterName, mainProjectPath, geoList = []):
         # Save skin weights.
         cmds.select(obj)
         bSkinSaver.bSaveSkinValues(weightFilePath)
+
+def loadSkinWeights(characterName, mainProjectPath, geoList = []):
+    # Get weight file paths.
+    weightFileDir = os.path.join(mainProjectPath, characterName, skinWeightsDir)
+    weightFiles = os.listdir(weightFileDir)
+
+    # Load skin weights.
+    for weightFile in weightFiles:
+        extRes = os.path.splitext(weightFile)
+
+        # Check extension format.
+        if not extRes > 1:
+            continue
+
+        # Check skin weight file.
+        if not extRes[1] == skinWeightsExt:
+            continue
+
+        # Check geometry list.
+        if geoList and not extRes[0] in geoList:
+            continue
+
+        # Check if the object exists.
+        if not cmds.objExists(extRes[0]):
+            continue
+
+        weightFileFullPath = os.path.join(weightFileDir, weightFile)
+        bSkinSaver.bLoadSkinValues(loadOnSelection = False, inputFile = weightFileFullPath)
