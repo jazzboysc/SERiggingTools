af3967bbfd60ab157425521b1a0a9223aa72312e
Che Sun <jazzboysc@gmail.com>
2020-01-31 10:59:54 +0800

    ....

--- Utils/SEDeformerHelper.py
+++ Utils/SEDeformerHelper.py
@@ -1,41 +1,8 @@
 import maya.cmds as cmds
+import maya.mel as mel
 
 #-----------------------------------------------------------------------------
-# This function doesn't work, do not use.
-def createSingleWrap(wrapped, wrapper):    
-    wrapperBase = cmds.duplicate(wrapper, n = wrapper + 'Base')
-    cmds.hide(wrapperBase)
-    
-    wrappedShape = cmds.listRelatives(wrapped, s = True)[0]
-    wrapperShape = cmds.listRelatives(wrapper, s = True)[0]
-    wrapperBaseShape = cmds.listRelatives(wrapperBase, s = True)[0]
-    
-    try:
-        cmds.addAttr(wrapper, ln = 'dropoff', at = 'float', k = 1, dv = 4.0, hasMinValue = True, min = 0.0, hasMaxValue = True, max = 20.0)
-    except:
-        pass
-    try:
-        cmds.addAttr(wrapper, ln = 'smoothness', at = 'float', k = 1, dv = 0.0, hasMinValue = True, min = 0.0)
-    except:
-        pass
-    try:
-        cmds.addAttr(wrapper, ln = 'influenceType', at = 'long', k = 0, dv = 2, hasMinValue = True, min = 1, hasMaxValue = True, max = 2)
-    except:
-        pass
-    
-    wrapNode = cmds.createNode('wrap')
-    
-    cmds.connectAttr(wrapperBaseShape + '.worldMesh[0]', wrapNode + '.basePoints[0]')
-    cmds.connectAttr(wrapperShape + '.worldMesh[0]', wrapNode + '.driverPoints[0]')
-    
-    cmds.connectAttr(wrapper + '.dropoff', wrapNode + '.dropoff[0]')
-    cmds.connectAttr(wrapper + '.smoothness', wrapNode + '.smoothness[0]')
-    cmds.connectAttr(wrapper + '.influenceType', wrapNode + '.inflType[0]')
-    
-    cmds.connectAttr(wrappedShape + '.worldMatrix[0]', wrapNode + '.geomMatrix')
-    cmds.connectAttr(wrapNode + '.outputGeometry[0]', wrappedShape + '.inMesh')
-#-----------------------------------------------------------------------------
-def createMirrorShapeAlongLocalAxis(sculptShape, baseShape, localAxis = 'x', newShape = ''):    
+def createMirrorShapeAlongLocalAxis(sculptShape, baseShape, localAxis = 'x', newShape = ''):
     baseWrapped = cmds.duplicate(baseShape)[0]
     baseWrapper = cmds.duplicate(baseShape)[0]
 
@@ -55,12 +22,8 @@ def createMirrorShapeAlongLocalAxis(sculptShape, baseShape, localAxis = 'x', new
     cmds.select(cl = True)
     cmds.select(baseWrapped)
     cmds.select(baseWrapper, add = True)
-    cmds.CreateWrap()
+    mel.eval('doWrapArgList "7" { "1", "0", "1", "2", "1", "1", "0", "0" }')
     cmds.select(cl = True)
-
-    # TODO:
-    # Cannot guarantee name is 'wrap1', hard coded wrap node name here is problematic.
-    cmds.setAttr('wrap1.exclusiveBind', 1)
 
     cmds.setAttr(tempBS + '.' + sculptShape, 1)
 
